4e6f0363c33a781d1f14a8ea78e9630be44d1921
Sourov <sourovchandra65@gmail.com>
2026-02-13 11:23:23 +0600

    sharedpreferences has done

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,108 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get getStartedWith => 'Get Started With';
+
+  @override
+  String get email => 'Email';
+
+  @override
+  String get password => 'Password';
+
+  @override
+  String get forgetAccount => 'Forget Password';
+
+  @override
+  String get doNotHaveAccount => 'Don\'t have an account ?';
+
+  @override
+  String get signUp => 'Sign Up';
+
+  @override
+  String get yourEmailAddress => 'Your Email Address';
+
+  @override
+  String get a6DigitVerificationPinWillSendToYourEmailAddress =>
+      'A 6 digit verification pin will send to your email address';
+
+  @override
+  String get haveAccount => 'Have Account ?';
+
+  @override
+  String get signIn => 'Sign In';
+
+  @override
+  String get pinVerification => 'PIN Verification';
+
+  @override
+  String get verify => 'Verify';
+
+  @override
+  String get setPassword => 'Set Password';
+
+  @override
+  String get minimumLengthPassword8CharacterWithLatterAndNumber =>
+      'Minimum length password 8 character with latter and number combination';
+
+  @override
+  String get confirmPassword => 'Confirm Password';
+
+  @override
+  String get confirm => 'Confirm';
+
+  @override
+  String get joinWithUp => 'Join With Us';
+
+  @override
+  String get firstName => 'First Name';
+
+  @override
+  String get lastName => 'Last Name';
+
+  @override
+  String get mobile => 'Mobile';
+
+  @override
+  String get cancelled => 'Cancelled';
+
+  @override
+  String get completed => 'Completed';
+
+  @override
+  String get progress => 'Progress';
+
+  @override
+  String get title => 'Title';
+
+  @override
+  String get description => 'Description';
+
+  @override
+  String get date => 'Date';
+
+  @override
+  String get sou => 'sou';
+
+  @override
+  String get addNewTask => 'Add New Task';
+
+  @override
+  String get subject => 'Subject';
+
+  @override
+  String get updateProfile => 'Update Profile';
+
+  @override
+  String get photo => 'photo';
+
+  @override
+  String get helloWorld => 'Hello World!';
+}
---
+++ lib/l10n/app_localizations_es.dart
@@ -0,0 +1,108 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Spanish Castilian (`es`).
+class AppLocalizationsEs extends AppLocalizations {
+  AppLocalizationsEs([String locale = 'es']) : super(locale);
+
+  @override
+  String get getStartedWith => 'শুরু করুন';
+
+  @override
+  String get email => 'ইমেইল';
+
+  @override
+  String get password => 'পাসওয়ার্ড';
+
+  @override
+  String get forgetAccount => 'অ্যাকাউন্ট ভুলে গেছেন';
+
+  @override
+  String get doNotHaveAccount => 'অ্যাকাউন্ট নেই ?';
+
+  @override
+  String get signUp => 'সাইন আপ';
+
+  @override
+  String get yourEmailAddress => 'আপনার ইমেইল ঠিকানা';
+
+  @override
+  String get a6DigitVerificationPinWillSendToYourEmailAddress =>
+      '৬ সংখ্যার ভেরিফিকেশন পিন আপনার ইমেইলে পাঠানো হবে';
+
+  @override
+  String get haveAccount => 'অ্যাকাউন্ট আছে ?';
+
+  @override
+  String get signIn => 'সাইন ইন';
+
+  @override
+  String get pinVerification => 'পিন যাচাইকরণ';
+
+  @override
+  String get verify => 'যাচাই করুন';
+
+  @override
+  String get setPassword => 'পাসওয়ার্ড সেট করুন';
+
+  @override
+  String get minimumLengthPassword8CharacterWithLatterAndNumber =>
+      'কমপক্ষে ৮ অক্ষরের পাসওয়ার্ড (অক্ষর ও সংখ্যা সহ)';
+
+  @override
+  String get confirmPassword => 'পাসওয়ার্ড নিশ্চিত করুন';
+
+  @override
+  String get confirm => 'নিশ্চিত করুন';
+
+  @override
+  String get joinWithUp => 'আমাদের সাথে যোগ দিন';
+
+  @override
+  String get firstName => 'নামের প্রথম অংশ';
+
+  @override
+  String get lastName => 'নামের শেষ অংশ';
+
+  @override
+  String get mobile => 'মোবাইল';
+
+  @override
+  String get cancelled => 'বাতিল';
+
+  @override
+  String get completed => 'সম্পন্ন';
+
+  @override
+  String get progress => 'চলমান';
+
+  @override
+  String get title => 'শিরোনাম';
+
+  @override
+  String get description => 'বর্ণনা';
+
+  @override
+  String get date => 'তারিখ';
+
+  @override
+  String get sou => 'সৌ';
+
+  @override
+  String get addNewTask => 'নতুন টাস্ক যোগ করুন';
+
+  @override
+  String get subject => 'বিষয়';
+
+  @override
+  String get updateProfile => 'প্রোফাইল আপডেট করুন';
+
+  @override
+  String get photo => 'ছবি';
+
+  @override
+  String get helloWorld => 'Hello World!';
+}
